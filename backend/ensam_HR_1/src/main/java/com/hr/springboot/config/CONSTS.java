a4238d2a7135fd70d7276163f184faff546ca797
Saad Hadiche <61362146+H-Saad@users.noreply.github.com>
2023-01-19 23:12:28 +0100

    Delete backend/ensam_HR_1 directory

--- backend/ensam_HR_1/src/main/java/com/hr/springboot/config/CONSTS.java
+++
@@ -1,9 +0,0 @@
-package com.hr.springboot.config;
-
-public interface CONSTS {
-	String USER_ROLE = "User";
-	String L3_ROLE = "layer3_Admin";
-	String L2_ROLE = "layer2_Admin";
-	String L1_ROLE = "layer1_Admin";
-	String SU_ROLE = "superuser";
-}
